c845dccaac3d9a743c6aa7af8da0c1d5fa557320
nugroho-s <massatriya@gmail.com>
2016-03-07 10:52:12 +0700

    added OS

---
+++ OS/OS.c
@@ -0,0 +1,26 @@
+#include <sys/types.h> 
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc,char* argv[]){
+	if (argc != 3){
+		printf("error, argument doesn't match\n");
+		exit(EXIT_FAILURE);
+	}
+	int jpage = atoi(argv[1]);
+	int jframe = atoi(argv[2]);
+	int pidOS = getpid();
+	char* command = "./mmu ";
+	char* buffer = malloc(50);
+	strcpy(buffer,command);
+	strcat(buffer,argv[1]);
+	strcat(buffer," W2 ");
+	char PID[5];
+	sprintf(PID, "%d", pidOS);
+	strcat(buffer,PID);
+	printf("%s\n",buffer);
+	system(buffer);
+	return 0;
+}
