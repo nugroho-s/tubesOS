2713b37aa9e92ac3c7795c6269cd44e275360a12
nugroho-s <massatriya@gmail.com>
2016-03-08 19:21:48 +0700

    added comment

--- OS/OS.c
+++ OS/OS.c
@@ -33,6 +33,7 @@ PageTable[Index].Requested);
 
 
 int main(int argc,char* argv[]){
+	//menggunakan 2 argumen tambahan, jumlah page dan jumlah frame secara berurutan
 	if (argc != 3){
 		printf("error, argument doesn't match\n");
 		exit(EXIT_FAILURE);
@@ -43,16 +44,19 @@ int main(int argc,char* argv[]){
 	int shmid;
 	int NumberOfPages = jpage;
 	printf("from OS %d\n",NumberOfPages);
+	//get shared memory id
 	if ((shmid = shmget(pidOS, NumberOfPages*sizeof(page_table_entry), IPC_CREAT|0666)) < 0) {
 		printf("1\n");
         perror("shmget");
         exit(1);
     }
+    //get shared memory (page table) pointer
     page_table_pointer shm;
     if ((shm = shmat(shmid, NULL, 0)) == (void*)-1) {
         perror("shmat");
         exit(1);
     }
+    //inisialisasi page table
     page_table_pointer s = shm;
     int i;
     for (i=0;i<NumberOfPages;i++){
@@ -61,6 +65,7 @@ int main(int argc,char* argv[]){
 		s[i].Dirty = 0;
 		s[i].Requested = 0;
 	}
+	//inisialisasi command untuk menjalankan MMU
 	char* command = "./MMU.ls ";
 	char* buffer = malloc(100);
 	strcpy(buffer,command);
